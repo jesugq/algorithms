3c9d76908e52a56b220150308a8e8d5abc79a5d4
jesugq <jesugq@proton.me>
2024-07-18 10:27:18 -0600

    Add JMC17 18

---
+++ JMC17/18 Even Digit Sum.java
@@ -0,0 +1,17 @@
+public class EvenDigitSum {
+    
+    public static int getEvenDigitSum(int number) {
+        
+        if (number < 0) return -1;
+        
+        int sum = 0;
+        
+        while (number > 0) {
+            
+            sum = (number%10 % 2 == 0) ? (sum + number%10) : sum;
+            number /= 10;
+        }
+        
+        return sum;
+    }
+}
