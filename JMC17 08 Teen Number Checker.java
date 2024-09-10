eb5ae2e7b1b7a9f3093e71263ff0ae37f8afe4ee
jesugq <jesugq@outlook.com>
2024-09-10 12:02:02 -0600

    Add JMC17 08

---
+++ JMC17 08 Teen Number Checker.java
@@ -0,0 +1,12 @@
+public class TeenNumberChecker {
+	
+    public static boolean hasTeen(int a, int b, int c) {
+	    
+        return isTeen(a) || isTeen(b) || isTeen(c);
+    }
+    
+    public static boolean isTeen(int age) {
+	    
+        return (age > 12) && (age < 20);
+    }
+}
